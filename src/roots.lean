ef202c8ef3e7e5499c0031c6d5756a79eca323d0
Oliver Nash <github@olivernash.org>
2022-12-02 17:50:39 +0000

    Add most of proof of `is_root_system.is_reduced_iff`
    Various required auxiliary lemmas also added. I apologise for mislabeling
    this as 'low effort'. This is much harder than I realised.

--- src/roots.lean
+++ src/roots.lean
@@ -87,6 +87,35 @@ module.to_pre_symmetry_apply_self $ h.coroot_apply_self_eq_two α
 lemma symmetry_of_root_sq (α : Φ) : (h.symmetry_of_root α)^2 = 1 :=
 sorry
 
+@[simp] lemma symmetry_of_root_image_subset (α : Φ) :
+  h.symmetry_of_root α '' Φ ⊆ Φ :=
+(classical.some_spec (h.exists_dual _ α.property)).2
+
+@[simp] lemma neg_mem (α : Φ) : - (α : V) ∈ Φ :=
+begin
+  have := (image_subset_iff.mp $ h.symmetry_of_root_image_subset α) α.property,
+  simpa only [subtype.val_eq_coe, mem_preimage, symmetry_of_root_apply_self_neg] using this,
+end
+
+@[simp] lemma coroot_image_subset_zmultiples (α : Φ) :
+  h.coroot α '' Φ ⊆ add_subgroup.zmultiples (1 : k) :=
+h.subset_zmultiples α (h.coroot α) ⟨h.coroot_apply_self_eq_two α, h.symmetry_of_root_image_subset α⟩
+
+@[simp] lemma coroot_apply_mem_zmultiples (α β : Φ) :
+  h.coroot α β ∈ add_subgroup.zmultiples (1 : k) :=
+begin
+  have := (image_subset_iff.mp $ h.coroot_image_subset_zmultiples α) β.property,
+  simpa using this,
+end
+
+lemma exists_int_coroot_apply_eq (α β : Φ) :
+  ∃ (n : ℤ), h.coroot α β = n :=
+begin
+  obtain ⟨n, hn⟩ := add_subgroup.mem_zmultiples_iff.mp (h.coroot_apply_mem_zmultiples α β),
+  rw ← hn,
+  exact ⟨n, by simp⟩,
+end
+
 lemma zero_not_mem : (0 : V) ∉ Φ :=
 λ contra, by simpa using h.coroot_apply_self_eq_two ⟨0, contra⟩
 
@@ -125,11 +154,45 @@ begin
   sorry,
 end
 
--- Estimate low effort.
 lemma is_reduced_iff (h : is_root_system k Φ) :
   is_reduced_root_system k Φ ↔ ∀ (α ∈ Φ) (t : k), t • α ∈ Φ → t = 1 ∨ t = -1 :=
 begin
-  sorry,
+  refine ⟨λ hr x hx t ht, _, λ hr, ⟨h, λ α hα contra, _⟩⟩,
+  { let α : Φ := ⟨x, hx⟩,
+    let β : Φ := ⟨t • x, ht⟩,
+    have ht₀ : t ≠ 0, { have := h.zero_not_mem, contrapose! this, rwa [this, zero_smul] at ht, },
+    have hαβ : t⁻¹ • (β : V) = α,
+    { rw [subtype.coe_mk, subtype.coe_mk, smul_smul, inv_mul_cancel ht₀, one_smul], },
+    obtain ⟨n, hn⟩ := h.exists_int_coroot_apply_eq β α,
+    have htn : t * n = 2,
+    { have : h.coroot β (t • α) = 2 := h.coroot_apply_self_eq_two β,
+      simpa only [linear_map.map_smulₛₗ, ring_hom.id_apply, algebra.id.smul_eq_mul, hn] using this },
+    obtain ⟨m, hm⟩ := h.exists_int_coroot_apply_eq α β,
+    have htm : t⁻¹ * m = 2,
+    { have : h.coroot α (t⁻¹ • β) = 2, { rw hαβ, exact h.coroot_apply_self_eq_two α, },
+      simpa only [linear_map.map_smulₛₗ, ring_hom.id_apply, algebra.id.smul_eq_mul, hm] using this },
+    have hmn : n * m = 4,
+    { have := congr_arg2 ((*) : k → k → k) htn htm,
+      rw [mul_mul_mul_comm, mul_inv_cancel ht₀, one_mul] at this,
+      norm_cast at this,
+      exact this, },
+    have hn : n ≠ 1,
+    { have := hr.two_smul_not_mem α α.property,
+      contrapose! this,
+      simp only [nsmul_eq_smul_cast k 2, nat.cast_two, subtype.coe_mk],
+      rw [this, algebra_map.coe_one, mul_one] at htn,
+      rwa htn at ht, },
+    have hn' : n ≠ -1,
+    { have := hr.two_smul_not_mem (-α) (h.neg_mem α),
+      contrapose! this,
+      simp only [nsmul_eq_smul_cast k 2, nat.cast_two, subtype.coe_mk, smul_neg],
+      rw [this, int.cast_neg, algebra_map.coe_one, mul_neg, mul_one, neg_eq_iff_neg_eq] at htn,
+      rwa [← htn, neg_smul] at ht, },
+    -- Similarly `m ≠ ± 1`. Using `hmn : n * m = 4` this means `n`, `m` both `± 2`, thus `t = ± 1`.
+    sorry, },
+  { replace contra : (2 : k) • α ∈ Φ, { rwa [nsmul_eq_smul_cast k 2 α, nat.cast_two] at contra, },
+    replace contra := hr α hα (2 : k) contra,
+    norm_num at contra, },
 end
 
 end is_root_system
