80d34f63ed42c2ac8f46963d4ea6137951126467
Oliver Nash <github@olivernash.org>
2023-02-25 12:31:23 +0000

    Fix definition of `is_root_system.is_base`.
    The condition that the base be a subset of the roots was missing!

--- src/roots.lean
+++ src/roots.lean
@@ -169,10 +169,10 @@ structure is_reduced_root_system (k : Type*) {V : Type*} [comm_ring k] [char_zer
 namespace is_root_system
 
 structure is_base {k V ι : Type*} [linear_ordered_field k] [add_comm_group V] [module k V]
-{Φ : set V} (h : is_root_system k Φ)
-(b : basis ι k V) : Prop :=
-(is_integral: ∀ (α ∈ Φ) i, b.coord i α ∈ add_subgroup.zmultiples (1 : k))
-(same_sign: ∀ (α ∈ Φ), (∀ i, 0 ≤ b.coord i α) ∨ (∀ i, b.coord i α ≤ 0))
+  {Φ : set V} (h : is_root_system k Φ) (b : basis ι k V) : Prop :=
+(subset : range (b : ι → V) ⊆ Φ)
+(is_integral : ∀ (α ∈ Φ) i, b.coord i α ∈ add_subgroup.zmultiples (1 : k))
+(same_sign : ∀ (α ∈ Φ), (∀ i, 0 ≤ b.coord i α) ∨ (∀ i, b.coord i α ≤ 0))
 
 section field
 
