7eb04f619e20fa0a8ff7f175326e301062c49e4d
DeeproChoudhury <72229274+DeeproChoudhury@users.noreply.github.com>
2023-03-09 18:18:53 +0000

    Add ker_to_dual_eq_bot for the proof of coroot_span_eq_top

--- src/roots.lean
+++ src/roots.lean
@@ -108,6 +108,7 @@ lemma unit.is_of_fin_order_of_finite_of_span_eq_top_of_image_subseteq
   {Φ : set V} (hΦ₁ : Φ.finite) (hΦ₂ : submodule.span k Φ = ⊤)
   {u : (End k V)ˣ} (hu : u '' Φ ⊆ Φ) : is_of_fin_order u :=
 begin
+
   sorry,
 end
 
@@ -242,6 +243,58 @@ begin
   exact ⟨ β, β.property, rfl⟩,
 end
 
+/-- The linear map `V → V⋆` induced by a root system. -/
+@[simps] def to_dual : V →ₗ[k] module.dual k V :=
+{ to_fun := λ x, ∑ᶠ α, αᘁ x • αᘁ,
+  map_add' := λ x y ,
+  begin
+    ext,
+    simp only [linear_map.map_add, map_add, linear_map.add_apply, add_smul],
+    rw finsum_add_distrib,
+    simp only [linear_map.add_apply],
+    { haveI : finite Φ := finite_coe_iff.mpr h.finite,
+      apply set.to_finite, },
+    { haveI : finite Φ := finite_coe_iff.mpr h.finite,
+      apply set.to_finite, },
+  end,
+  map_smul' :=
+  begin
+    intros c x,
+    ext,
+    simp only [linear_map.map_smulₛₗ, ring_hom.id_apply, algebra.id.smul_eq_mul,
+      linear_map.smul_apply, ←smul_smul],
+    rw [← smul_finsum],
+    simp only [linear_map.smul_apply, algebra.id.smul_eq_mul],
+  end }
+
+lemma to_dual_apply_apply (x y : V) :
+  h.to_dual x y = ∑ᶠ α, αᘁ x • αᘁ y :=
+begin
+ haveI h2 : finite Φ := finite_coe_iff.mpr h.finite,
+ have h3 : (support (λ (α : ↥Φ), (αᘁ) x • αᘁ)).finite, by apply set.to_finite,
+ change (∑ᶠ (α : Φ), (αᘁ x) • αᘁ) y = _,
+ letI : fintype Φ := fintype.of_finite ↥Φ,
+ rw finsum_eq_finset_sum_of_support_subset _ (_ : _ ⊆ ↑(finset.univ : finset Φ)),
+ rw finsum_eq_finset_sum_of_support_subset _ (_ : _ ⊆ ↑(finset.univ : finset Φ)),
+ { simp only [linear_map.coe_fn_sum, fintype.sum_apply, linear_map.smul_apply], },
+ { simp only [finset.coe_univ, subset_univ], },
+ { simp only [finset.coe_univ, support_subset_iff, mem_univ, implies_true_iff], },
+end
+
+/-- The bilinear map on `V` induced by a root system. -/
+def to_bilinear_map : V →ₗ[k] V →ₗ[k] k :=
+{ to_fun := λ x, h.to_dual x,
+  map_add' := λ x y, by { ext, simp only [map_add], },
+  map_smul' := λ c x, by { ext, simp only [linear_map.map_smulₛₗ], } }
+
+/-- The bilinear form on `V` induced by a root system. -/
+def to_bilin_form : bilin_form k V := h.to_bilinear_map.to_bilin
+
+/-- This corresponds to the bilinear form on V induced by the root system being nonsingular -/
+lemma ker_to_dual_eq_bot : h.to_dual.ker = ⊥ :=
+sorry
+
+
 @[simp] lemma coroot_symmetry_apply_eq (α β : Φ) (h') :
   ⟨ട α β, h'⟩ᘁ = βᘁ - (βᘁ α) • αᘁ :=
 begin
@@ -276,7 +329,16 @@ lemma finite_coroots : (range h.coroot).finite :=
 @set.finite_range _ _ h.coroot $ finite_coe_iff.mpr h.finite
 
 lemma coroot_span_eq_top : submodule.span k (range h.coroot) = ⊤ :=
-sorry
+begin
+  suffices : ∀ (v : V) (h' : ∀ (α : Φ), h.coroot α v = 0), v = 0,
+  {
+    sorry,
+  },
+  intros v hv,
+  -- use `ker_to_dual_eq_bot`
+  
+  sorry,
+end
 
 lemma root_ne_zero (α : Φ) : (α : V) ≠ 0 :=
 begin
@@ -321,7 +383,6 @@ lemma is_root_system_coroots : is_root_system k $ range h.coroot :=
   end,
   subset_zmultiples :=
   begin
-
     rintros aux ⟨α, rfl⟩ α' ⟨h₁, h₂⟩ - ⟨-, ⟨β, rfl⟩, rfl⟩,
     have hα : h.coroot α ≠ 0, {
         intro h,
@@ -466,52 +527,6 @@ end
 the Wyel group is a subgroup of the symmetry group
 subgroups closure induction-/
 
-/-- The linear map `V → V⋆` induced by a root system. -/
-@[simps] def to_dual : V →ₗ[k] module.dual k V :=
-{ to_fun := λ x, ∑ᶠ α, αᘁ x • αᘁ,
-  map_add' := λ x y ,
-  begin
-    ext,
-    simp only [linear_map.map_add, map_add, linear_map.add_apply, add_smul],
-    rw finsum_add_distrib,
-    simp only [linear_map.add_apply],
-    { haveI : finite Φ := finite_coe_iff.mpr h.finite,
-      apply set.to_finite, },
-    { haveI : finite Φ := finite_coe_iff.mpr h.finite,
-      apply set.to_finite, },
-  end,
-  map_smul' :=
-  begin
-    intros c x,
-    ext,
-    simp only [linear_map.map_smulₛₗ, ring_hom.id_apply, algebra.id.smul_eq_mul,
-      linear_map.smul_apply, ←smul_smul],
-    rw [← smul_finsum],
-    simp only [linear_map.smul_apply, algebra.id.smul_eq_mul],
-  end }
-
-lemma to_dual_apply_apply (x y : V) :
-  h.to_dual x y = ∑ᶠ α, αᘁ x • αᘁ y :=
-begin
- haveI h2 : finite Φ := finite_coe_iff.mpr h.finite,
- have h3 : (support (λ (α : ↥Φ), (αᘁ) x • αᘁ)).finite, by apply set.to_finite,
- change (∑ᶠ (α : Φ), (αᘁ x) • αᘁ) y = _,
- letI : fintype Φ := fintype.of_finite ↥Φ,
- rw finsum_eq_finset_sum_of_support_subset _ (_ : _ ⊆ ↑(finset.univ : finset Φ)),
- rw finsum_eq_finset_sum_of_support_subset _ (_ : _ ⊆ ↑(finset.univ : finset Φ)),
- { simp only [linear_map.coe_fn_sum, fintype.sum_apply, linear_map.smul_apply], },
- { simp only [finset.coe_univ, subset_univ], },
- { simp only [finset.coe_univ, support_subset_iff, mem_univ, implies_true_iff], },
-end
-
-/-- The bilinear map on `V` induced by a root system. -/
-def to_bilinear_map : V →ₗ[k] V →ₗ[k] k :=
-{ to_fun := λ x, h.to_dual x,
-  map_add' := λ x y, by { ext, simp only [map_add], },
-  map_smul' := λ c x, by { ext, simp only [linear_map.map_smulₛₗ], } }
-
-/-- The bilinear form on `V` induced by a root system. -/
-def to_bilin_form : bilin_form k V := h.to_bilinear_map.to_bilin
 
 -- Bilinear form is dot product, Weyl group is subgroup generated by reflections in the roots,
 -- Reflection preserve lengths and angles
@@ -552,7 +567,7 @@ begin
 end
 
 omit h
-theorem foo {k : Type u_1} {V : Type u_2} (n m : ℤ)
+theorem foo {k : Type*} {V : Type*} (n m : ℤ)
   [field k]
   [char_zero k]
   [add_comm_group V]
@@ -584,7 +599,7 @@ begin
 end
 
 include h
-theorem m_not_neg_1 {k : Type u_1} {V : Type u_2} (n m : ℤ)
+theorem m_not_neg_1 {k : Type*} {V : Type*} (n m : ℤ)
   [field k]
   [char_zero k]
   [add_comm_group V]
